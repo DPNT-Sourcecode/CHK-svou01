755f7bc8e27c25856512c1d645f1e7b2615951bc
sbx_user1051 <sbx_user1051@169.254.126.165>
2024-06-07 12:52:34 +0000

    Fri Jun 07 12:52:34 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,7 @@
 
 from dataclasses import dataclass
 from typing import Optional, Callable
-from frozenlist import FrozenList
+# from frozenlist import FrozenList
 import math
 from functools import cache
 import line_profiler
@@ -11,16 +11,16 @@ import line_profiler
 Quantities = dict[str, int]
 
 
-@dataclass(frozen=True)
+@dataclass
 class Offer:
     requires_quantities: Quantities
     includes: Quantities
     price: int
 
 
-Deal = FrozenList[Offer]
+Deal = list[Offer]
 
-@cache
+# @cache
 @line_profiler.profile
 def quantities_geq(lhs: Quantities, rhs: Quantities) -> bool:
     lhs
@@ -57,51 +57,49 @@ def buy_n_get_m_free(sku: str, quantity: int, reward_sku: str, reward_quantity: 
             price
         )
 
-OFFERS = frozenset(
-    [
-        basic_price("A", 50),
-        bulk_discount("A", 3, 130),
-        bulk_discount("A", 5, 200),
-        basic_price("B", 30),
-        bulk_discount("B", 2, 45),
-        basic_price("C", 20),
-        basic_price("D", 15),
-        basic_price("E", 40),
-        buy_n_get_m_free("E", 2, "B", 1, 80),
-        basic_price("F", 10),
-        buy_n_get_m_free("F", 2, "F", 1, 20),
-        basic_price("G", 20),
-        basic_price("H", 10),
-        bulk_discount("H", 5, 45),
-        bulk_discount("H", 10, 80),
-        basic_price("I", 35),
-        basic_price("J", 60),
-        basic_price("K", 80),
-        bulk_discount("K", 2, 150),
-        basic_price("L", 90),
-        basic_price("M", 15),
-        basic_price("N", 40),
-        buy_n_get_m_free("N", 3, "M", 1, 120),
-        basic_price("O", 10),
-        basic_price("P", 50),
-        bulk_discount("P", 5, 200),
-        basic_price("Q", 30),
-        bulk_discount("Q", 3, 80),
-        basic_price("R", 50),
-        buy_n_get_m_free("R", 3, "Q", 1, 150),
-        basic_price("S", 30),
-        basic_price("T", 20),
-        basic_price("U", 40),
-        buy_n_get_m_free("U", 3, "U", 1, 120),
-        basic_price("V", 50),
-        bulk_discount("V", 2, 90),
-        bulk_discount("V", 3, 130),
-        basic_price("W", 20),
-        basic_price("X", 90),
-        basic_price("Y", 10),
-        basic_price("Z", 50),
-    ]
-)
+OFFERS = [
+    basic_price("A", 50),
+    bulk_discount("A", 3, 130),
+    bulk_discount("A", 5, 200),
+    basic_price("B", 30),
+    bulk_discount("B", 2, 45),
+    basic_price("C", 20),
+    basic_price("D", 15),
+    basic_price("E", 40),
+    buy_n_get_m_free("E", 2, "B", 1, 80),
+    basic_price("F", 10),
+    buy_n_get_m_free("F", 2, "F", 1, 20),
+    basic_price("G", 20),
+    basic_price("H", 10),
+    bulk_discount("H", 5, 45),
+    bulk_discount("H", 10, 80),
+    basic_price("I", 35),
+    basic_price("J", 60),
+    basic_price("K", 80),
+    bulk_discount("K", 2, 150),
+    basic_price("L", 90),
+    basic_price("M", 15),
+    basic_price("N", 40),
+    buy_n_get_m_free("N", 3, "M", 1, 120),
+    basic_price("O", 10),
+    basic_price("P", 50),
+    bulk_discount("P", 5, 200),
+    basic_price("Q", 30),
+    bulk_discount("Q", 3, 80),
+    basic_price("R", 50),
+    buy_n_get_m_free("R", 3, "Q", 1, 150),
+    basic_price("S", 30),
+    basic_price("T", 20),
+    basic_price("U", 40),
+    buy_n_get_m_free("U", 3, "U", 1, 120),
+    basic_price("V", 50),
+    bulk_discount("V", 2, 90),
+    bulk_discount("V", 3, 130),
+    basic_price("W", 20),
+    basic_price("X", 90),
+    basic_price("Y", 10),
+    basic_price("Z", 50),
+]
 
 
 def get_deal_price(deal: Deal) -> int:
@@ -113,22 +111,20 @@ def get_quantities(skus: str) -> Quantities:
         if sku not in quantities:
             quantities[sku] = 0
         quantities[sku] += 1
-    return frozendict(quantities)
+    return quantities
 
 
-@cache
+# @cache
 @line_profiler.profile
 def find_best_deal(
     quantities: Quantities,
     *,
-    offers: frozenset[Offer] = OFFERS
+    offers: list[Offer] = OFFERS
 ) -> Optional[Deal]:
     if all(quantity == 0 for quantity in quantities.values()):
-        empty = FrozenList([])
-        empty.freeze()
-        return empty
+        return []
 
-    applicable_offers = set(offer for offer in offers if quantities_geq(quantities, offer.requires_quantities))
+    applicable_offers = (offer for offer in offers if quantities_geq(quantities, offer.requires_quantities))
 
     best_deal = None
     best_price = math.inf
@@ -139,14 +135,12 @@ def find_best_deal(
                 new_quantities[included_sku] = max(
                     0, new_quantities[included_sku] - included_quantity
                 )
-        new_quantities = frozendict(new_quantities)
 
         rest_of_deal = find_best_deal(new_quantities)
         if rest_of_deal is None:
             continue
 
-        new_deal = FrozenList([offer, *rest_of_deal])
-        new_deal.freeze()
+        new_deal = [offer, *rest_of_deal]
 
         if (new_deal_price := get_deal_price(new_deal)) < best_price:
             best_price = new_deal_price
@@ -155,7 +149,7 @@ def find_best_deal(
     return best_deal
 
 
-def checkout(skus: str, *, offers: frozenset[Offer] = OFFERS):
+def checkout(skus: str, *, offers: list[Offer] = OFFERS):
     best_deal = find_best_deal(
         get_quantities(skus),
         offers=offers
