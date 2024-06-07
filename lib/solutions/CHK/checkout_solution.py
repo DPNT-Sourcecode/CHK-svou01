904ac76766964e98db2e4938735c44c159a36898
sbx_user1051 <sbx_user1051@169.254.126.165>
2024-06-07 10:47:19 +0000

    Fri Jun 07 10:47:19 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,12 +3,30 @@
 
 from dataclasses import dataclass
 
-@dataclass
+@dataclass(frozen=True)
 class PricePoint:
     sku: str
     quantity: int
     price: int
 
+PRICE_POINTS = set(
+    PricePoint('A', 1, 50),
+    PricePoint('A', 3, 130),
+    PricePoint('B', 1, 30),
+    PricePoint('B', 2, 45),
+    PricePoint('C', 1, 20),
+    PricePoint('D', 1, 15),
+)
+
+def best_price_point(sku: str, quantity: int) -> PricePoint:
+    """
+    Finds the best price for the given SKU, given that
+    the customer is buying at least `quantity` of the item.
+    """
+    price_points_for_sku = set(pp for pp in PRICE_POINTS if pp.sku == sku and pp.quantity >= quantity)
+    return min(price_points_for_sku, key=lambda pp: pp.price)
+
 def checkout(skus):
     raise NotImplementedError()
+
 
