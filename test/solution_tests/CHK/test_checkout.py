f083ada01f414e382cbd3988aa9b0237542ae686
sbx_user1051 <sbx_user1051@169.254.126.165>
2024-06-07 11:33:07 +0000

    Fri Jun 07 11:33:07 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -4,11 +4,29 @@ from frozendict import frozendict
 class TestCheckout():
     def test_quantities_geq(self):
       assert checkout_solution.quantities_geq(
-        frozendict({"A": })
+        frozendict({"A": 3}),
+        frozendict({"A": 1}),
       )
+      assert not checkout_solution.quantities_geq(
+        frozendict({"A": 3}),
+        frozendict({"B": 1}),
+      )
+      assert checkout_solution.quantities_geq(
+        frozendict({"A": 3, "B": 1}),
+        frozendict({"A": 1}),
+      )
+      assert checkout_solution.quantities_geq(
+        frozendict({"A": 1, "B": 1}),
+        frozendict({"A": 1}),
+      )
+    
+    def test_offers_qualify(self):
+      assert Offer()
+      
 
     def test_checkout(self):
       assert checkout_solution.checkout("AAABADC") == 130 + 30 + 50 + 15 + 20 
 
     def test_checkout_invalid_input(self):
       assert checkout_solution.checkout("a") == -1 
+
