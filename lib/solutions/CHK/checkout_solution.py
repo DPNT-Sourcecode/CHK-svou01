70af920efff4a4028cdd9c0d1f463635354e7ee7
sbx_user1051 <sbx_user1051@169.254.75.93>
2024-06-07 15:40:11 +0000

    Fri Jun 07 15:40:11 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -40,53 +40,47 @@ BASIC_PRICES = {
     "Z": 50,
 }
 
-OFFERS = set(
-    [
-        bulk_discount("A", 3, 130),
-        bulk_discount("A", 5, 200),
-        
-        bulk_discount("B", 2, 45),
-        
-        
-        
-        buy_n_get_m_free("E", 2, "B", 1, 80),
-        
-        buy_n_get_m_free("F", 2, "F", 1, 20),
-        
-        
-        bulk_discount("H", 5, 45),
-        bulk_discount("H", 10, 80),
-        
-        
-        
-        bulk_discount("K", 2, 150),
-        
-        
-        
-        buy_n_get_m_free("N", 3, "M", 1, 120),
-        
-        
-        bulk_discount("P", 5, 200),
-        
-        bulk_discount("Q", 3, 80),
-        
-        buy_n_get_m_free("R", 3, "Q", 1, 150),
-        
-        
-        
-        buy_n_get_m_free("U", 3, "U", 1, 120),
-        
-        bulk_discount("V", 2, 90),
-        bulk_discount("V", 3, 130),
-        
-        
-        
-        
-    ]
-)
+BULK_DISCOUNTS = {
+    "A": {
+        3: 130,
+        5: 200
+    },
+    "B": {
+        2: 45
+    },
+    "H": {
+        5: 45,
+        10: 80
+    },
+    "K": {
+        2: 150
+    },
+    "P": {
+        5: 200
+    },
+    "Q": {
+        3: 80
+    },
+    "V": {
+        2: 90,
+        3: 130
+    },
+}
+
+BUY_N_GET_M_FREE = {
+    "E": {
+        2: {"B": 1}
+    },
+    "F": {
+        2: {"F": 1}
+    }
+}
 
 def checkout(skus: str) -> int:
     quantities = get_quantities(skus)
+
+    for sku, offer in buy_n_get_m_free.items():
+        if 
 
 if __name__ == "__main__":
     print(checkout("A"))
